954e8a0e56e61d526400104d9149d8fe4641e732
norristown <jarvasity@gmail.com>
2023-12-14 12:46:09 -0500

    disabled autofocus on page load for search input

--- src/app/components/utilities/Search.jsx
+++ src/app/components/utilities/Search.jsx
@@ -8,17 +8,16 @@ function Search({ userInput, setUserInput }) {
 
   useEffect(() => {
     if (inputRef.current) {
-      inputRef.current.focus();
+      inputRef.current.blur();
     }
   }, []);
   return (
     <input
-      className="mt-32 border-2 border-zinc-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+      className="mt-32 mx-4 border-2 border-zinc-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
       type="text"
       placeholder="search..."
       value={userInput}
       onChange={(e) => setUserInput(e.target.value)}
-      autoFocus={false}
       ref={inputRef}
     ></input>
   );
